31fb8d3e4e9cd850011f3b0ea2b91d40966b8117
Mizzauro <tailsx81@yahoo.es>
2026-01-25 18:51:38 -0400

    barrita del amor uwu (de corazones)

--- Assets/Jugador/Código/Health.cs
+++ Assets/Jugador/Código/Health.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 using System.Collections.Generic;
-
-public enum HeartEnum
-{
-    RedHeart = 1,
-    HalfRedHeart = 2,
-    SoulHeart = 3
-
-}
+using UnityEngine.UIElements;
 
 public class Health : MonoBehaviour
 {
-
     public int maxHearts = 6;
     public int currentHearts = 4;
-
-    public GameObject heartbarUI;
+    public int soulHearts = 0;
+    public GameObject healthbarUI;
 
     private Mediador mediador;
+    private VisualElement container;
+
+    [SerializeField] private VisualTreeAsset heartFullTemplate;
+    [SerializeField] private VisualTreeAsset heartHalfTemplate;
+    [SerializeField] private VisualTreeAsset heartEmptyTemplate;
+    [SerializeField] private VisualTreeAsset heartSoulTemplate;
+    [SerializeField] private VisualTreeAsset heartSoulHalfTemplate;
+    
     
     public void Configuration(Mediador mediadorRecibido)
     {
         mediador = mediadorRecibido;
+        var root = healthbarUI.GetComponent<UIDocument>().rootVisualElement;
+        container = root.Q<VisualElement>("HealthBar");
+        DrawHealth();
     }
 
     //public List<Heart> heartList = new List<Heart>();
@@ -40,5 +43,37 @@ public class Health : MonoBehaviour
 
         return(false);
     }
+    public void DrawHealth()
+    {
+        container.Clear();
+        for (int i = 2; i < currentHearts; i += 2)
+        {
+            VisualElement heart = heartFullTemplate.Instantiate();
+            container.Add(heart);
+        }
 
+        if (currentHearts % 2 != 0)
+        {
+            VisualElement halfHeart = heartHalfTemplate.Instantiate();
+            container.Add(halfHeart);
+        }
+
+        for (int i = currentHearts + 1; i < maxHearts; i += 2)
+        {
+            VisualElement emptyHeart = heartEmptyTemplate.Instantiate();
+            container.Add(emptyHeart);
+        }
+
+        for (int i = 2; i < soulHearts; i += 2)
+        {
+            VisualElement soulHeart = heartSoulTemplate.Instantiate();
+            container.Add(soulHeart);
+        }
+
+        for (int i = 0; i < soulHearts % 2; i++)
+        {
+            VisualElement halfSoulHeart = heartSoulHalfTemplate.Instantiate();
+            container.Add(halfSoulHeart);
+        }
+    }
 }
